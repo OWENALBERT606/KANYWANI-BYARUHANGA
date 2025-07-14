d48c1422661455c23341c470287e39c448c4af84
owenalbert606 <owenalbert606@gmail.com>
2025-07-14 21:08:54 +0300

    gg

--- app/(home)/blog/page.tsx
+++ app/(home)/blog/page.tsx
@@ -1,11 +1,12 @@
 
 import UnderDevelopmentPage from '@/components/john/new-hom'
+import Other from '@/components/john/other'
 import React from 'react'
 
 export default function Page() {
   return (
     <div>
-      <UnderDevelopmentPage/>
+      <Other/>
     </div>
   )
 }
