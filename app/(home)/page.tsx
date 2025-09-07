017022c5ab00038895a50b95637e7c35df477fae
Owenalbert606 <owenalbert606@gmail.com>
2025-09-07 14:31:31 +0300

    adding hero

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import UnderDevelopmentPage from '../../components/john/new-hom'
 import { HeroSlider } from '../../components/hero-slider'
+import CustomCarousel from '@/components/slider'
 
 export default function Page() {
   return (
     <div>
-     <div className=""> <HeroSlider/></div>
+    <CustomCarousel/>
+     {/* <div className=""> <HeroSlider/></div> */}
       <UnderDevelopmentPage/>
     </div>
   )
